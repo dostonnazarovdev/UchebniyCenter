b733a2e9466847348fe04f94fee45ef44347bfa3
DostonNazarov <dostondev4477@gmail.com>
2024-05-10 17:24:26 +0430

    added Controllers & Repositories & Services

--- UchebniyCenter/Program.cs
+++ UchebniyCenter/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EducationCenterDAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,9 +15,34 @@ namespace UchebniyCenter
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+
+        Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
+            using (var db = new DatabaseContext())
+            {
+                db.StudentModels.Add(new StudentModel()
+                {
+                    FIO = "aaabbb",
+                    Adress = "jizzax",
+                    Phone = "12345",
+                    ParentFIO = "ddddd",
+                    Remark = "ttttttttt",
+                    Status = (StudentStatus)Enum.Parse(typeof(StudentStatus), "Active", true),
+                    IsFirstLessonFreeUsed = true
+            }) ;
+               
+                db.SaveChanges();
+
+                foreach (var student in db.StudentModels)
+                {
+                    Console.WriteLine(student.FIO);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
